46ad053978aa90ba72e2b6e70a86d77280fde4e9
chetantarale <chetan@shippable.com>
2016-01-20 18:05:51 +0530

    update Mongo class to MongoClient

--- db.php
+++ db.php
@@ -3,7 +3,7 @@ class MongoDbConnection {
   public function __construct() {
     $mongoUrl = getenv("MONGOLAB_URI");
     $dbName = substr(parse_url($mongoUrl)["path"], 1);
-    $this->mongo = new Mongo($mongoUrl);
+    $this->mongo = new MongoClient($mongoUrl);
     $this->scores = $this->mongo->{$dbName}->scores;
   }
 
